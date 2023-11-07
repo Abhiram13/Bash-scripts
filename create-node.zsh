e0c8f302b290f8c710feac735e88d90a43a6a951
Abhiram13 <abhiramnagadi@gmail.com>
2023-11-07 15:25:58 +0530

    updating scripts, added gitignore, env

--- create-node.zsh
+++ create-node.zsh
@@ -5,22 +5,28 @@ tsconfig_path=`cat /Users/abhiramnagadi/Documents/NodeJS\ projects/Payroll/tscon
 val=""
 
 echo $(pwd "$0")
+current_directory=$(basename $(pwd))
 
 read -e -p 'Name of the project ' val
 echo $val
 
-# checking if $var is empty, then appending default random value
-if [ -z "$val" ]; then
-   echo "here"
-   val=$folder
+# checking if $var is not empty, then going inside that folder
+if [ -n "$val" ]; then
+   mkdir $val #create folder with given name
+   cd $val # go to that folder
 fi
 
-echo $val
+# checking if $var is empty, then appending directory name
+if [ -z "$val" ]; then
+   val=$current_directory
+fi
 
-mkdir $val #create folder with given name
-cd $val # go to that folder
 npm init -y # initialize node project
 npm i typescript nodemon ts-node-dev # install required modules
 npm i # install node_modules
-touch index.ts # create index.ts file
+
+pkg=$(sed -i '' -r '$s/}/,\n"scripts":{\n"test": "echo \\\"Error: no test specified\\\" \&\& exit 1\",\n"build": "npm install \& tsc",\n"node": "ts-node-dev --respawn --transpile-only index.ts",\n"start": "tsc \& node build\/index.js"\n}\n}/' "package.json")
+echo "console.log('Hello from Project: $val')" > index.ts
 echo "$tsconfig_path" > tsconfig.json # create and copy contents into tsconfig.json
+echo "/node_modules\n/.DS_Store\n/.env\n/.logs\n/build" > .gitignore
+touch .env
